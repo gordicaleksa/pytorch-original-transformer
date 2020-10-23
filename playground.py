9e6edf4dfeac363c2dc7cd6e5a06d02d44bd29bb
Aleksa Gordic <gordicaleksa@gmail.com>
2020-10-23 22:35:41 +0200

    Analyzing the annotated transformer

--- playground.py
+++ playground.py
@@ -1,2 +1,28 @@
+import math
+
+
+import torch
+from torch import nn
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+
+
 if __name__ == "__main__":
     print('Place explanations of potentially confusing stuff in this file')

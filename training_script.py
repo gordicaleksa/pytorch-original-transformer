9f2cbde1784d392321ef1c94f0151d0ecfffa80b
Aleksa Gordic <gordicaleksa@gmail.com>
2020-11-01 19:25:13 +0100

    Refactor training script

--- training_script.py
+++ training_script.py
@@ -21,9 +21,80 @@ import utils.utils as utils
 from utils.constants import *
 
 
+num_of_trg_tokens_processed = 0
+global_train_step, global_val_step = [0, 0]
+writer = SummaryWriter()  # (tensorboard) writer will output to ./runs/ directory by default
+
+
+# Simple decorator function so that I don't have to pass these arguments every time I call get_train_val_loop
+def get_train_val_loop(baseline_transformer, custom_lr_optimizer, kl_div_loss, label_smoothing, pad_token_id, time_start):
+
+    def train_val_loop(is_train, token_ids_loader, epoch):
+        global num_of_trg_tokens_processed, global_train_step, global_val_step, writer
+
+        if is_train:
+            baseline_transformer.train()
+        else:
+            baseline_transformer.eval()
+
+        device = next(baseline_transformer.parameters()).device
+
+        #
+        # Main loop - start of the core part
+        #
+        for batch_idx, token_ids_batch in enumerate(token_ids_loader):
+            src_token_ids_batch, trg_token_ids_batch_input, trg_token_ids_batch_gt = fetch_src_and_trg_batches(token_ids_batch)
+            src_mask, trg_mask, num_src_tokens, num_trg_tokens = build_masks_and_count_tokens(src_token_ids_batch, trg_token_ids_batch_input, pad_token_id, device)
+
+            # log because the KL loss expects log probabilities (just an implementation detail)
+            predicted_log_distributions = baseline_transformer(src_token_ids_batch, trg_token_ids_batch_input, src_mask, trg_mask)
+            target_distributions = label_smoothing(trg_token_ids_batch_gt)
+
+            if is_train:
+                custom_lr_optimizer.zero_grad()  # clean the gradients of every trainable weight in the computational graph
+
+            loss = kl_div_loss(predicted_log_distributions, target_distributions)
+
+            if is_train:
+                loss.backward()  # compute the gradients for every trainable weight in the computational graph
+                custom_lr_optimizer.step()  # apply the gradients to weights
+
+            # End of core part
+
+            #
+            # Logging and metrics
+            #
+            # todo: add BLEU
+            # todo: gradient clipping for stability?
+            if is_train:
+                global_train_step += 1
+                num_of_trg_tokens_processed += num_trg_tokens
+
+                if training_config['enable_tensorboard']:
+                    writer.add_scalar('training_loss', loss.item(), global_train_step)
+
+                if training_config['console_log_freq'] is not None and batch_idx % training_config['console_log_freq'] == 0:
+                    print(f'Transformer training: time elapsed= {(time.time() - time_start):.2f} [s] '
+                          f'| epoch={epoch + 1} | batch= [{batch_idx + 1}] '
+                          f'| target tokens/batch= {num_of_trg_tokens_processed / training_config["console_log_freq"]}')
+
+                    num_of_trg_tokens_processed = 0
+
+                # Save model checkpoint
+                if training_config['checkpoint_freq'] is not None and (epoch + 1) % training_config['checkpoint_freq'] == 0 and batch_idx == 0:
+                    ckpt_model_name = f"transformer_ckpt_epoch_{epoch + 1}.pth"
+                    torch.save(utils.get_training_state(training_config, baseline_transformer), os.path.join(CHECKPOINTS_PATH, ckpt_model_name))
+            else:
+                global_val_step += 1
+
+                if training_config['enable_tensorboard']:
+                    writer.add_scalar('val_loss', loss.item(), global_val_step)
+
+    return train_val_loop
+
+
 # todo: after I setup the whole train/val loop step through and make sure that attention mechanism, etc. works as expected
 def train_transformer(training_config):
-    writer = SummaryWriter()  # (tensorboard) writer will output to ./runs/ directory by default
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # checking whether you have a GPU
 
     # Step 1: Prepare data loaders and data-related information
@@ -46,7 +117,7 @@ def train_transformer(training_config):
     # Step 3: Prepare other training related utilities
     kl_div_loss = nn.KLDivLoss(reduction='batchmean')
 
-    smoother = LabelSmoothingDistribution(BASELINE_MODEL_LABEL_SMOOTHING_VALUE, pad_token_id, trg_vocab_size, device)
+    label_smoothing = LabelSmoothingDistribution(BASELINE_MODEL_LABEL_SMOOTHING_VALUE, pad_token_id, trg_vocab_size, device)
 
     custom_lr_optimizer = CustomLRAdamOptimizer(
                 Adam(baseline_transformer.parameters(), betas=(0.9, 0.98), eps=1e-9),
@@ -54,79 +125,17 @@ def train_transformer(training_config):
                 training_config['num_warmup_steps']
             )
 
-    # Step 4: Start the training
-    train_loss_data = []
-    val_loss_data = []
-    num_of_trg_tokens_processed = 0
+    # The decorator function makes things cleaner since there is a lot of redundancy between the train and val loops
+    train_val_loop = get_train_val_loop(baseline_transformer, custom_lr_optimizer, kl_div_loss, label_smoothing, pad_token_id, time.time())
 
-    ts = time.time()
-    global_train_step, global_val_step = [0, 0]
+    # Step 4: Start the training
     for epoch in range(training_config['num_of_epochs']):
         # Training loop
-        baseline_transformer.train()
-        for batch_idx, token_ids_batch in enumerate(train_token_ids_loader):
-            src_token_ids_batch, trg_token_ids_batch_input, trg_token_ids_batch_gt = fetch_src_and_trg_batches(token_ids_batch)
-            src_mask, trg_mask, num_src_tokens, num_trg_tokens = build_masks_and_count_tokens(src_token_ids_batch, trg_token_ids_batch_input, pad_token_id, device)
-
-            # log because the KL loss expects log probabilities (just an implementation detail)
-            predicted_log_distributions = baseline_transformer(src_token_ids_batch, trg_token_ids_batch_input, src_mask, trg_mask)
-            target_distributions = smoother(trg_token_ids_batch_gt)
-
-            custom_lr_optimizer.zero_grad()  # clean the gradients of every trainable weight in the computational graph
-            loss = kl_div_loss(predicted_log_distributions, target_distributions)
-            loss.backward()  # compute the gradients for every trainable weight in the computational graph
-            custom_lr_optimizer.step()  # apply the gradients to weights
-
-            #
-            # Logging and checkpoint creation
-            #
-
-            # todo: add BLEU
-            # todo: gradient clipping for stability?
-            train_loss_data.append(loss.item())
-            num_of_trg_tokens_processed += num_trg_tokens
-            global_train_step += 1
-
-            if training_config['enable_tensorboard']:
-                writer.add_scalar('training_loss', loss.item(), global_train_step)
-
-            if training_config['console_log_freq'] is not None and batch_idx % training_config['console_log_freq'] == 0:
-                print(f'Transformer training: time elapsed= {(time.time() - ts):.2f} [s] '
-                      f'| epoch={epoch + 1} | batch= [{batch_idx + 1}] '
-                      f'| target tokens/batch= {num_of_trg_tokens_processed/training_config["console_log_freq"]}')
-
-                num_of_trg_tokens_processed = 0
-
-            # Save model checkpoint
-            if training_config['checkpoint_freq'] is not None and (epoch + 1) % training_config['checkpoint_freq'] == 0 and batch_idx == 0:
-                ckpt_model_name = f"transformer_ckpt_epoch_{epoch + 1}.pth"
-                torch.save(utils.get_training_state(training_config, baseline_transformer), os.path.join(CHECKPOINTS_PATH, ckpt_model_name))
+        train_val_loop(is_train=True, token_ids_loader=train_token_ids_loader, epoch=epoch)
 
         # Validation loop
-        baseline_transformer.eval()
         with torch.no_grad():
-            for batch_idx, token_ids_batch in enumerate(val_token_ids_loader):
-                src_token_ids_batch, trg_token_ids_batch_input, trg_token_ids_batch_gt = fetch_src_and_trg_batches(token_ids_batch)
-                src_mask, trg_mask, num_src_tokens, num_trg_tokens = build_masks_and_count_tokens(src_token_ids_batch, trg_token_ids_batch_input, pad_token_id)
-
-                # Push to GPU (if we have a GPU on this machine)
-                src_token_ids_batch = src_token_ids_batch.to(device)
-                trg_token_ids_batch_input = trg_token_ids_batch_input.to(device)
-                trg_token_ids_batch_gt = trg_token_ids_batch_gt.to(device)
-                src_mask = src_mask.to(device)
-                trg_mask = trg_mask.to(device)
-
-                # log because the KL loss expects log probabilities (just an implementation detail)
-                predicted_log_distributions = baseline_transformer(src_token_ids_batch, trg_token_ids_batch_input, src_mask, trg_mask)
-                target_distributions = smoother(trg_token_ids_batch_gt)
-
-                loss = kl_div_loss(predicted_log_distributions, target_distributions)
-                val_loss_data.append(loss.item())
-
-                global_val_step += 1
-
-                if training_config['enable_tensorboard']:
-                    writer.add_scalar('val_loss', loss.item(), global_val_step)
+            train_val_loop(is_train=False, token_ids_loader=val_token_ids_loader, epoch=epoch)
 
     # Save the latest generator in the binaries directory
     torch.save(utils.get_training_state(training_config, baseline_transformer), os.path.join(BINARIES_PATH, utils.get_available_binary_name()))
